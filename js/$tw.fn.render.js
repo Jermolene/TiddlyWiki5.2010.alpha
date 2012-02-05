921b7f4b302cb73f217ad94b0050da3623ca2541
Jeremy Ruston <jeremy@osmosoft.com>
2012-02-05 15:06:31 +0000

    Removed all the files from the old TiddlyWiki5 alpha prototype released in 2010
    These files will be preserved in the archive directory of the new repo

--- js/$tw.fn.render.js
+++
@@ -1,9 +0,0 @@
-$tw.fn.render = function(options) {
-	var defaults = {
-		displayPoint: "body"
-	};
-	options = $.extend({},defaults,options);
-	this.each(function(i) {
-		$tw.display.renderTiddler(this).appendTo($(options.displayPoint));
-	});
-};
--- js/$tw.link.js
+++
@@ -1,9 +0,0 @@
-$tw.link = {};
-
-(function() {
-
-	$tw.link.create = function(targetTitle,containingTiddler) {
-		return new $tw.macros.link("to:" + targetTitle,{tiddler: containingTiddler});
-	};
-	
-})();
